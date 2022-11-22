834a3d5133e6fc419f79fba445f1f813de61b7c2
kistef <76614331+kistef@users.noreply.github.com>
2022-11-22 22:01:04 +0100

    Add files via upload

---
+++ src/Input.js
@@ -0,0 +1,39 @@
+import {Component} from "react";
+import React from "react";
+
+/* with a text field and button so that we can send our messages*/
+
+class Input extends Component {
+  state = {
+    text: ""
+  }
+
+  onChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({text: ""});
+    this.props.onSendMessage(this.state.text);
+  }
+
+  render() {
+    return (
+      <div className="Input">
+        <form onSubmit={e => this.onSubmit(e)}>
+          <input
+            onChange={e => this.onChange(e)}
+            value={this.state.text}
+            type="text"
+            placeholder="Enter your message and press ENTER"
+            autoFocus={true}
+          />
+          <button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Input;
---
+++ src/Messages.js
@@ -0,0 +1,43 @@
+import {Component} from "react";
+import React from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+/* will be a list of messages*/
+
+class Messages extends Component {
+  render() {
+    const {messages} = this.props;
+    return (
+      <ul className="Messages-list">
+        {messages.map(m => this.renderMessage(m))}
+      </ul>
+    );
+  }
+
+  renderMessage(message) {
+    const {member, text} = message;
+    const msgKey = uuidv4(); 
+    const {currentMember} = this.props;
+    const messageFromMe = member.id === currentMember.id;
+    const className = messageFromMe ?
+      "Messages-message currentMember" : "Messages-message";
+    return (
+      <div key={msgKey}>
+      <li  className={className}>
+      <span
+        
+        className="avatar"
+        style={{backgroundColor: member.clientData.color}}
+      />
+        <div className="Message-content">
+          <div className="username">
+            {member.clientData.username}
+          </div>
+          <div className="text">{text}</div>
+        </div>
+      </li></div>
+    );
+  }
+}
+
+export default Messages;
